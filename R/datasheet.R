33ff7c6bf29220ea55a1e110cb9e1b4c205fa232
benharden27 <benharden27@gmail.com>
2020-01-21 14:40:15 -0500

    update neuston datasheet reading to include calculating tow distance, biodensity, and moon phase

--- R/datasheet.R
+++ R/datasheet.R
@@ -31,36 +31,54 @@ create_datasheet <- function(data_input, summary_input, data_type = "CTD", csv_o
     data <- compile_neuston(data, elg_input)
   }
 
-  # TODO: Remove columns we don't need, e.g. deployment
+  # Remove columns we don't need, e.g. deployment
+  data <- dplyr::select(data, -deployment)
 
+  # export to csv
   if(!is.null(csv_output)) {
     readr::write_csv(format_csv_output(data),csv_output, na = "")
   }
 
   return(data)
+
 }
 
+#' @export
+#' @rdname compile_neuston
 compile_neuston <- function(data, elg_input) {
 
+  # WORK OUT TOW DISTANCE AND ASSIGN TOW LENGTH
   # find out time_out in UTC
-  time_diff <-   time_length(lubridate::hms(data$time_out) - lubridate::hms(data$time_in), unit = "minute")
+  time_diff <- lubridate::time_length(lubridate::hms(data$time_out) - lubridate::hms(data$time_in), unit= "minutes")
   time_diff[time_diff<0] <- time_diff[time_diff<0] + 60 * 24
   data <- dplyr::mutate(data,dttm_out = dttm + lubridate::minutes(time_diff))
 
   # read in elg
   elg <- get_elg(elg_input)
 
-
+  # find nearest points and calculate along-path distance
   sti <- find_near(elg$dttm, data$dttm)
   eni <- find_near(elg$dttm, data$dttm_out)
-  data <- dplyr::mutate(data, dist = NA)
+  tow_length <- rep(NA, length(sti))
   for (i in 1:length(sti)) {
-    data$dist[i] <- tail(
+    tow_length[i] <- tail(
       oce::geodDist(
         elg$lon[sti[i]:eni[i]],
         elg$lat[sti[i]:eni[i]],
         alongPath = TRUE),1)
   }
+  data <- dplyr::mutate(data, tow_length = tow_length)
+
+  # calculate biodensity
+  data <- dplyr::mutate(data, biodens = biomass/tow_length)
+
+  # Calculate moon data
+  moon_data <- oce::moonAngle(data$dttm,data$lon,data$lat)
+
+  # Add moon info to dataset
+  data <- dplyr::mutate(data,
+                        moon_phase = moon_data$illuminatedFraction * 100,
+                        moon_risen = moon_data$altitude > 0)
 
   return(data)
 }
