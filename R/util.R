29934fae0b3b693f8271f35238ae4b6dd47d4ee2
benharden27 <benharden27@gmail.com>
2021-11-03 19:44:15 -0400

    add wind speed and wind direction conversion utility tools

--- R/util.R
+++ R/util.R
@@ -21,3 +21,43 @@ find_near <- function(vec,vals) {
   return(locs)
 
 }
+
+
+#' Convert wind speed and wind direction to u and v
+#'
+#' @param ws input wind speed vector
+#' @param wd input wind direction vector
+#'
+#' @return
+#' @export
+#'
+#' @examples
+wswd_to_uv <- function(ws,wd) {
+
+  v = -ws*cos(wd*pi/180)
+  u = -ws*sin(wd*pi/180)
+
+  out <- tibble::tibble(u = u, v = v)
+
+}
+
+
+#' U and V to wind speed and wind direction
+#'
+#' @param u
+#' @param v
+#'
+#' @return
+#' @export
+#'
+#' @examples
+uv_to_wswd <- function(u,v) {
+
+  ws <- sqrt(u^2+v^2)
+  wd <- 90 - atan2(v,u)*180/pi
+  wd[wd<0&!is.na(wd)] <- wd[wd<0&!is.na(wd)] + 360
+
+  out <- tibble::tibble(ws = ws, wd = wd)
+
+  return(out)
+}
