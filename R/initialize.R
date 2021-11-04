4dc3f0ec85361dbe5d97343bcebef109ce771985
benharden27 <benharden27@gmail.com>
2021-11-03 21:27:29 -0400

    add custom project capabilities with template folder copy and rename to cruise name

---
+++ R/initialize.R
@@ -0,0 +1,25 @@
+# Initialize a cruise
+initialize_cruise <- function(path, ...) {
+
+  # ensure path exists but dont overwrite project
+  if(!dir.exists(path)) {
+    dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  } else {
+    stop("Project already exists")
+  }
+
+  # copy contents of template directory to project folder
+  file.copy(list.files(system.file("extdata","initial_dir",package="seaprocess"), full.names = TRUE),
+            path,
+            recursive = TRUE)
+
+  ## TODO add cruise metadata contents
+
+  ## TODO append cruise ID to all files
+  cruise <- basename(path)
+  old_names <- list.files(path, full.names = T, recursive = T)
+  new_names <- file.path(dirname(old_names),paste0(cruise,"_",basename(old_names)))
+  file.rename(old_names, new_names)
+
+
+}
