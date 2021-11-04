cbb8a65401394b787ab95a0ced889bc8824edb4a
benharden27 <benharden27@gmail.com>
2021-11-04 12:32:56 -0400

    refine processing of data to make use of local dirs

--- R/process.R
+++ R/process.R
@@ -2,20 +2,41 @@
 # a complete data archive and odv output
 #
 
-process_adcp <- function(adcp_folder, output_csv = NULL, output_odv = NULL,
-                         cruiseID = NULL, ...) {
+#' Master processing function for ADCP data
+#'
+#' @param adcp_folder
+#' @param csv_folder
+#' @param odv_folder
+#' @param cruiseID
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+process_adcp <- function(adcp_folder, cruiseID = NULL,
+                         csv_folder = "output/csv", csv_filename = "adcp.csv",
+                         odv_folder = "output/odv/adcp", odv_filename = "adcp.txt",
+                         add_cruiseID = TRUE, ...) {
 
   # Read in all adcp file in the folder
   adcp <- read_adcp_fold(adcp_folder, ...)
 
-  # Convert adcp to csv output
+  # write adcp data to csv
+  # add cruise ID to default output if not already existing
+  if(add_cruiseID) {
+    csv_filename <- add_file_cruiseID(csv_filename, cruiseID)
+  }
+  # write file
+  safely_write_csv(adcp, file.path(csv_folder,csv_filename))
 
-  if(!is.null(output_odv)) {
-    format_adcp_odv(adcp, output_odv, cruiseID = cruiseID)
+  # Convert adcp to odv output
+  if(!is.null(odv_folder)) {
+    format_adcp_odv(adcp, file.path(odv_folder,odv_filename), cruiseID = cruiseID)
   }
 }
 
-#' Title
+#' Master processing function for event data
 #'
 #' @param elg_folder
 #' @param output_file
@@ -24,8 +45,10 @@ process_adcp <- function(adcp_folder, output_csv = NULL, output_odv = NULL,
 #' @export
 #'
 #' @examples
-process_elg <- function(elg_folder, output_csv = NULL, output_odv = NULL,
-                        average_window = 60, cruiseID = NULL, ... ) {
+process_elg <- function(elg_folder, cruiseID = NULL,
+                        csv_folder = "output/csv", csv_filename = "elg.csv",
+                        odv_folder = "output/odv/elg", odv_filename = "elg.txt",
+                        add_cruiseID = TRUE, average_window = 60, ...) {
 
   # Read in all the ELG files in the folder
   elg <- read_elg_fold(elg_folder, ...)
@@ -36,18 +59,69 @@ process_elg <- function(elg_folder, output_csv = NULL, output_odv = NULL,
   }
 
   # Output csv file
-  if(!is.null(output_csv)) {
-    output <- purrr::safely(readr::write_csv)(elg, output_csv)
+  # add cruise ID to default output if not already existing
+  if(add_cruiseID) {
+    csv_filename <- add_file_cruiseID(csv_filename, cruiseID)
+  }
+  # write file
+  safely_write_csv(elg, file.path(csv_folder,csv_filename))
+
+  # output odv file
+  if(!is.null(odv_folder)) {
+    format_elg_odv(elg, file.path(odv_folder,odv_filename), cruiseID = cruiseID)
+  }
+
+  return(elg)
+
+}
+
+#' Master processing function for ctd data
+#'
+#' @param ctd_folder
+#' @param csv_folder
+#' @param odv_folder
+#' @param cruiseID
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+process_ctd <- function(ctd_folder, cruiseID = NULL,
+                        csv_folder = "output/csv", csv_filename = "ctd.csv",
+                        odv_folder = "output/odv/ctd", odv_filename = "ctd.txt",
+                        add_cruiseID = TRUE, ...) {
+
+  #
+  ctd <- read_ctd_fold(ctd_folder, cruiseID = cruiseID, ...)
+  if(add_cruiseID) {
+    csv_filename <- add_file_cruiseID(csv_filename, cruiseID)
+  }
+  safely_write_csv(ctd, file.path(csv_folder,csv_filename))
+
+  ## TODO CTD odv output
+
+}
+
+
+safely_write_csv <- function(data, csv_filepath = NULL) {
+  print(csv_filepath)
+  if(!is.null(csv_filepath)) {
+    output <- purrr::safely(readr::write_csv)(data, csv_filepath)
     if(!is.null(output$error)) {
+      print(output$error)
       message("Couldn't export the data to a csv file. Most likely specified directory doesn't exist")
     }
   }
 
-  # output odv file
-  if(!is.null(output_odv)) {
-    format_elg_odv(elg, output_odv, cruiseID = cruiseID)
+}
+
+add_file_cruiseID <- function(filename, cruiseID) {
+
+  if(!is.null(filename) & !is.null(cruiseID)) {
+    filename <- paste0(cruiseID,"_",filename)
   }
 
-  return(elg)
+  return(filename)
 
 }
