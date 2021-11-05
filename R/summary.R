53d74ed7f35dc68cacc816669ef5c7b5001edd8f
benharden27 <benharden27@gmail.com>
2021-11-05 14:41:23 -0400

    edit summary for readability

--- R/summary.R
+++ R/summary.R
@@ -28,6 +28,7 @@
 #' * Location
 #' * Surface Temperature
 #' * Surface Salinity
+#' * etc.
 #'
 #' The event data is typically stored in a file with an extension elg. [read_elg()] deals with reading this data in and formatting it properly.
 #'
@@ -40,7 +41,7 @@
 #' summary_input <- system.file("extdata", "S285_station.xlsx", package="seaprocess")
 #' elg_input <- system.file("extdata", "S285_elg", package="seaprocess")
 #' summary <- create_summary(summary_input, elg_input)
-create_summary <- function(summary_input, elg_input, csv_output = NULL) {
+create_summary <- function(summary_input, elg_input, csv_output = NULL, force_stations = FALSE, ...) {
 
   # read in the summary_input xlsx file
   summary <- readxl::read_excel(summary_input, col_types = "text")
@@ -60,12 +61,37 @@ create_summary <- function(summary_input, elg_input, csv_output = NULL) {
   # TODO: add fail safe to ensure that times are actually close
   ii <- find_near(elg$dttm, summary$dttm)
 
+  # Check to see if the time difference is greater than 60 seconds
+  diff_time <- elg$dttm[ii] - summary$dttm
+  iii <- which(diff_time > 60)
+  if(length(iii) > 0) {
+    message <- paste("The following stations are more than 60 seconds from nearest elg reading:",
+                     paste0(summary$station[iii], "-", summary$deployment[iii], ": ", diff_time[[iii]], " seconds"),
+                     sep = "\n")
+
+    if(force_stations) {
+      warning(paste(message,
+              "Setting the station metadata to NA and continuing to run. Run create_summary() without force_stations = TRUE to force a stop here",
+              sep = "\n"))
+      ii[iii] <- NA
+    } else {
+      stop(paste(message,
+                 "Please enter the metadata from paper datasheet by hand into the input summary datasheet and rerun",
+                 sep = "\n"))
+    }
+  }
+
+
   # extract these values and add to the right of summary
-  # TODO: make the ourputs selectible when you run the function
+  # TODO: make the outputs selectible when you run the function
   elg_to_add <- dplyr::select(elg[ii,], lon, lat, temp, sal, fluor)
   summary <- dplyr::bind_cols(summary,elg_to_add)
 
-  # TODO: add checks to ensure that there are no duplicate deployments for any one station
+  # check to ensure that there are no duplicate deployments for any one station
+  duplicated_deployments <- dplyr::n_groups(dplyr::group_by(summary, station, deployment)) != nrow(summary)
+  if(duplicated_deployments) {
+    warning("Summary sheet has defined mulitple of the same deployment for the same station")
+  }
 
   # Output to csv file as long as the folder name exists and the extension is correct
   if(!is.null(csv_output)) {
@@ -73,10 +99,10 @@ create_summary <- function(summary_input, elg_input, csv_output = NULL) {
     ext <- tools::file_ext(csv_output)
     # test to see if the extension is csv or CSV
     if(stringr::str_detect("csv|CSV", ext)) {
-      # test to see if the filename directory is already in existance
+      # test to see if the filename directory is already in existence
       if(dir.exists(dirname(csv_output))) {
         # create csv output at csv_output location
-        readr::write_csv(format_csv_output(summary),csv_output)
+        readr::write_csv(format_csv_output(summary, ...),csv_output)
       } else {
         stop("csv_output does not direct towards a valid existing folder")
       }
@@ -103,12 +129,13 @@ create_summary <- function(summary_input, elg_input, csv_output = NULL) {
 #' @export
 #'
 #' @examples
-format_csv_output <- function(df, dttm_format = "%Y-%m-%dT%H:%M", dttm_suffix = "Z", ll_dec = 4, temp_dec = 2, sal_dec = 3, fluor_dec = 2) {
+format_csv_output <- function(df, dttm_format = "%Y-%m-%dT%H:%M", dttm_suffix = "Z",
+                              ll_dec = 4, temp_dec = 2, sal_dec = 3, fluor_dec = 2) {
 
   # Format date and time to be in ISO 8601 format including timezone
-  if("zd" %in% colnames(df) & "dttm" %in%colnames(df))
+  if("zd" %in% colnames(df) & "dttm" %in%colnames(df)) {
     df$dttm <- paste0(format(df$dttm - lubridate::hours(df$zd), dttm_format), zd_to_tz(df$zd))
-
+  }
 
   # TODO: create list of all column names and default resolutions
 
